e7e685e13c23d05bd9a7b9efc5795472a5ea78ae
gmbarroso <barroso.guilherme@gmail.com>
2020-07-02 09:02:14 -0300

    managing the useState

--- weather/src/containers/Home/index.js
+++ weather/src/containers/Home/index.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
+import { Button } from 'react-bootstrap';
+
 import Card from '../../components/CardComponent'
+import Clock from '../../components/ClockComponent'
+import Timer from '../../components/Timer'
 
 import {
   withRouter,
@@ -13,63 +17,95 @@ import './style.css'
 const enhance = compose(withRouter)
 
 const Home = () => {
-  const [ firstCity, setFirstCity ] = useState(null);
+  const [ comment, setComment ] = useState(null)
+  const [ min, setMin ] = useState(null)
+  const [ max, setMax ] = useState(null)
+  const [ prec, setPrec ] = useState(null)
+  const [ prob, setProb ] = useState(null)
+  const [ moon, setMoon ] = useState(null)
+  const [ seconds, setSeconds ] = useState(10800)
+  const [ reseted, setReseted ] = useState(false)
 
-  const transformResult = (result) => {
-    const { DailyForecasts } = result
-    const forecast = DailyForecasts[0]
-    // const hasPrecipitation = () => {
-    //   const day = forecast.Day.HasPrecipitation ? true : false
-      
-    //   return day
-    // 
-
-    let firstCityForecast = {
-      comment: forecast.Day.IconPhrase,
-      min: forecast.Temperature.Minimum.Value,
-      max: forecast.Temperature.Maximum.Value,
-    }
+  const handleTimer = (isReseted) => {
+    setSeconds(10800)
+    setReseted(true)
     
-    return firstCityForecast
+    if (isReseted) {
+      console.log(reseted)
+      clearInterval(seconds)
+      setReseted(false)
+    }
   }
 
   useEffect(() => {
     const token = 'IuVWje9ULZCICrZhuBv4PCuLF4sGEX6P'
     const br = 'pt-br'
+    const details = 'true'
     const metric = 'true'
     const location = '45881'
     const forecast = async () => {
-      const result = await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${location}?apikey=${token}&language=${br}&metric=${metric}`)
+      const result = await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${location}?apikey=${token}&language=${br}&details=${details}&metric=${metric}`)
       return result.json()
     }
 
-    if (firstCity === null) {
+    if (comment === null) {
       const result = forecast()
       result.then(value => {
-        setFirstCity(transformResult(value))
+        const forecast = value.DailyForecasts[0]
+
+        console.log(forecast)
+
+        setComment(forecast.Day.IconPhrase)
+        setMin(forecast.Temperature.Minimum.Value)
+        setMax(forecast.Temperature.Maximum.Value)
+        setPrec(forecast.Day.RainProbability)
+        setProb(forecast.Day.RainProbability)
+        setMoon(forecast.Moon)
       })
     }
-  }, [firstCity])
-
-  console.log('lalalalala', firstCity)
+  }, [comment, min, max, prec, prob])
   return (
     <div className="home">
+        <Clock />
       <div className="card-container">
         <Card
           cityName = "São Paulo"
-          // weatherState = { firstCity.comment }
-          // minTemp = { firstCity.min }
-          // maxTemp = { firstCity.max }
-          rainPrec = "25"
-          rainProb = "67"
+          weatherState = { comment }
+          minTemp = { min }
+          maxTemp = { max }
+          rainPrec = { prec }
+          rainProb = { prob }
+          moon = { moon }
         />
         <Card
-          cityName = ''
+          cityName = "São Paulo"
+          weatherState = { comment }
+          minTemp = { min }
+          maxTemp = { max }
+          rainPrec = { prec }
+          rainProb = { prob }
         />
         <Card
-          cityName = ''
+          cityName = "São Paulo"
+          weatherState = { comment }
+          minTemp = { min }
+          maxTemp = { max }
+          rainPrec = { prec }
+          rainProb = { prob }
         />
       </div>
+      <div>
+        <Timer
+          isReseted = { reseted }
+          seconds = { seconds }
+        />
+      </div>
+        <Button
+          onClick = {handleTimer}
+          variant="primary"
+        >
+            Atualizar agora
+        </Button>
     </div>
   )
 }

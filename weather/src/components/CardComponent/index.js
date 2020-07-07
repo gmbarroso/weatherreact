7aa8e575e3cea59a6ea61d4393831ddbbae3a745
gbarroso84 <gbarroso@bionexo.com>
2020-07-07 11:57:47 -0300

    increment the CardComponent

--- weather/src/components/CardComponent/index.js
+++ weather/src/components/CardComponent/index.js
@@ -1,9 +1,28 @@
 import React, { Fragment } from 'react';
-
+import { WiDaySunny } from "weather-icons-react";
 import { Card } from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css'
+
+// const handleMoonPhase = moon => {
+//   const phase = moon
+
+//   console.log('halo', typeof phase)
+// }
+
+const handleWeatherIcons = (moon) => {
+  const time = new Date()
+  const hour = time.getHours()
+
+  // handleMoonPhase(moon)
+
+  if (hour > 5 && hour < 18) {
+    return <WiDaySunny size={100}/>
+  }
+
+  return null
+}
 
 const CardComponent = ({
     cityName,
@@ -11,7 +30,8 @@ const CardComponent = ({
     maxTemp,
     rainProb,
     rainPrec,
-    weatherState
+    weatherState,
+    moon,
 }) => (
     <Card className="card-size">
       <Card.Body>
@@ -19,11 +39,14 @@ const CardComponent = ({
         <Card.Subtitle className="mb-2 text-muted">{weatherState}</Card.Subtitle>
         <Fragment>
           <div className="temperatures">
-            <span className="max">Máx: {maxTemp}ºC</span>
-            <span className="min">Mín: {minTemp}ºC</span>
+            <div className="weatherStatus">
+              {handleWeatherIcons(moon)}
+            </div>      
+            <span className="max">Máx: {maxTemp} ºC</span>
+            <span className="min">Mín: {minTemp} ºC</span>
           </div>
           <div>
-            Chuva: {rainPrec}mm - {rainProb}%
+            Chuva: {rainPrec} mm - {rainProb} %
           </div>
         </Fragment>
       </Card.Body>

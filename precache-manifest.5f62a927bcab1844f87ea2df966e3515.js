a9ff92764d9956ae8a307769bafcdd09737a1e22
gmbarroso <barroso.guilherme@gmail.com>
2020-08-01 19:01:16 -0300

    Updates

---
+++ precache-manifest.5f62a927bcab1844f87ea2df966e3515.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e87188f2679248b408d7f3164a93dc",
+    "url": "/weatherreact/index.html"
+  },
+  {
+    "revision": "fadf4ea75c851cc0343b",
+    "url": "/weatherreact/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4d981c75cf82ed4d5100",
+    "url": "/weatherreact/static/css/main.146e3cc3.chunk.css"
+  },
+  {
+    "revision": "fadf4ea75c851cc0343b",
+    "url": "/weatherreact/static/js/2.77047fca.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weatherreact/static/js/2.77047fca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d981c75cf82ed4d5100",
+    "url": "/weatherreact/static/js/main.97a171e2.chunk.js"
+  },
+  {
+    "revision": "daab2c413398db54295c",
+    "url": "/weatherreact/static/js/runtime-main.ed187f85.js"
+  },
+  {
+    "revision": "39ac499c2a7856b24ffe3ffde4c8cc61",
+    "url": "/weatherreact/static/media/15-s.39ac499c.png"
+  },
+  {
+    "revision": "0cf9cad1347244da8a9e197998e0eff9",
+    "url": "/weatherreact/static/media/16-s.0cf9cad1.png"
+  },
+  {
+    "revision": "3c85da3e400a073c51b3ea73bdf5a130",
+    "url": "/weatherreact/static/media/17-s.3c85da3e.png"
+  },
+  {
+    "revision": "421c13f1b1eb0ae947bb64472fec6baa",
+    "url": "/weatherreact/static/media/41-s.421c13f1.png"
+  },
+  {
+    "revision": "87032851c3532c9dd64f20f4bee155a9",
+    "url": "/weatherreact/static/media/br.87032851.svg"
+  },
+  {
+    "revision": "50623e6a761b392b5381ce35e8a77f99",
+    "url": "/weatherreact/static/media/es.50623e6a.svg"
+  },
+  {
+    "revision": "adf0099bf9c41909ce7d8b33e63a0978",
+    "url": "/weatherreact/static/media/github-logo.adf0099b.png"
+  },
+  {
+    "revision": "ae65659236a7e348402799477237e6fa",
+    "url": "/weatherreact/static/media/us.ae656592.svg"
+  },
+  {
+    "revision": "679b6cacf6f2f666a3cdd6ffe6ea824e",
+    "url": "/weatherreact/static/media/warning.679b6cac.png"
+  }
+]);

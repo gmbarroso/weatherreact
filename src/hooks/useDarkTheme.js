9d809e336bf3ee56a53bdfe3d1129dfc3522105a
gmbarroso <barroso.guilherme@gmail.com>
2020-10-16 16:10:16 -0300

    change darkMode

--- src/hooks/useDarkTheme.js
+++ src/hooks/useDarkTheme.js
@@ -2,24 +2,27 @@ import { useEffect } from 'react'
 
 const useDarkTheme = (checked) => {
     useEffect(() => {
+        const stylePeriodArray = document.getElementsByClassName('period')
         if (checked) {
             document.body.style.backgroundColor = '#282c34'
             document.body.style.color = '#fff'
             document.getElementsByClassName('card-size card')[0].style.backgroundColor = '#A9A9A9'
             document.getElementsByClassName('card-size card')[1].style.backgroundColor = '#A9A9A9'
             document.getElementsByClassName('card-size card')[2].style.backgroundColor = '#A9A9A9'
-            document.getElementsByClassName('period')[0].style.color = '#505050'
-            document.getElementsByClassName('period')[1].style.color = '#505050'
-            document.getElementsByClassName('period')[2].style.color = '#505050'
-            document.getElementsByClassName('subtitle')[0].style.color = '#505050'
-            document.getElementsByClassName('subtitle')[1].style.color = '#505050'
-            document.getElementsByClassName('subtitle')[2].style.color = '#505050'
-            document.getElementsByClassName('weatherState')[0].style.color = '#505050'
-            document.getElementsByClassName('weatherState')[1].style.color = '#505050'
-            document.getElementsByClassName('weatherState')[2].style.color = '#505050'
-            document.getElementsByClassName('min')[0].style.color = '#505050'
-            document.getElementsByClassName('min')[1].style.color = '#505050'
-            document.getElementsByClassName('min')[2].style.color = '#505050'
+            if (stylePeriodArray.length !== 0) {
+                document.getElementsByClassName('period')[0].style.color = '#505050'
+                document.getElementsByClassName('period')[1].style.color = '#505050'
+                document.getElementsByClassName('period')[2].style.color = '#505050'
+                document.getElementsByClassName('subtitle')[0].style.color = '#505050'
+                document.getElementsByClassName('subtitle')[1].style.color = '#505050'
+                document.getElementsByClassName('subtitle')[2].style.color = '#505050'
+                document.getElementsByClassName('weatherState')[0].style.color = '#505050'
+                document.getElementsByClassName('weatherState')[1].style.color = '#505050'
+                document.getElementsByClassName('weatherState')[2].style.color = '#505050'
+                document.getElementsByClassName('min')[0].style.color = '#505050'
+                document.getElementsByClassName('min')[1].style.color = '#505050'
+                document.getElementsByClassName('min')[2].style.color = '#505050'
+            }
         }
 
         return () => {
@@ -28,18 +31,20 @@ const useDarkTheme = (checked) => {
             document.getElementsByClassName('card-size card')[0].style.backgroundColor = 'white'
             document.getElementsByClassName('card-size card')[1].style.backgroundColor = 'white'
             document.getElementsByClassName('card-size card')[2].style.backgroundColor = 'white'
-            document.getElementsByClassName('period')[0].style.color = '#6c757d'
-            document.getElementsByClassName('period')[1].style.color = '#6c757d'
-            document.getElementsByClassName('period')[2].style.color = '#6c757d'
-            document.getElementsByClassName('subtitle')[0].style.color = '#6c757d'
-            document.getElementsByClassName('subtitle')[1].style.color = '#6c757d'
-            document.getElementsByClassName('subtitle')[2].style.color = '#6c757d'
-            document.getElementsByClassName('weatherState')[0].style.color = '#505050'
-            document.getElementsByClassName('weatherState')[1].style.color = '#505050'
-            document.getElementsByClassName('weatherState')[2].style.color = '#505050'
-            document.getElementsByClassName('min')[0].style.color = '#505050'
-            document.getElementsByClassName('min')[1].style.color = '#505050'
-            document.getElementsByClassName('min')[2].style.color = '#505050'
+            if (stylePeriodArray.length !== 0) {
+                document.getElementsByClassName('period')[0].style.color = '#6c757d'
+                document.getElementsByClassName('period')[1].style.color = '#6c757d'
+                document.getElementsByClassName('period')[2].style.color = '#6c757d'
+                document.getElementsByClassName('subtitle')[0].style.color = '#6c757d'
+                document.getElementsByClassName('subtitle')[1].style.color = '#6c757d'
+                document.getElementsByClassName('subtitle')[2].style.color = '#6c757d'
+                document.getElementsByClassName('weatherState')[0].style.color = '#505050'
+                document.getElementsByClassName('weatherState')[1].style.color = '#505050'
+                document.getElementsByClassName('weatherState')[2].style.color = '#505050'
+                document.getElementsByClassName('min')[0].style.color = '#505050'
+                document.getElementsByClassName('min')[1].style.color = '#505050'
+                document.getElementsByClassName('min')[2].style.color = '#505050'
+            }
 
         }
     })
